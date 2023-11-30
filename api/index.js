e25b08a19271cb237811eb603d2439dbf0f008aa
Alex Baldwin <alexbaldwin@gmail.com>
2023-11-30 16:16:04 -0600

    Default size is 88 and palettes are randomized

--- api/index.js
+++ api/index.js
@@ -2,14 +2,15 @@ const React = require("react");
 const { renderToString } = require("react-dom/server");
 const Avatar = require("boring-avatars").default;
 
+// const DEFAULT_COLORS = [ "#000", "#EFEFEF", "#000", "#FFF", "#000", ].join(",");
+
 const DEFAULT_COLORS = [
-  "#92A1C6",
-  "#146A7C",
-  "#F0AB3D",
-  "#C271B4",
-  "#C20D90",
-].join(",");
-const DEFAULT_SIZE = 80;
+  ["#000", "#000", "#000", "#000", "#000"].join(","),
+  ["#7C17F8", "#5200FF", "#E3B5F9", "#F720FF", "#7B8DF5"].join(","),
+  // Add more color arrays as needed
+];
+
+const DEFAULT_SIZE = 88;
 const DEFAULT_VARIANT = "marble";
 
 const VALID_VARIANTS = new Set([
@@ -37,11 +38,15 @@ app.get("/favicon.ico", (req, res) => {
   res.sendStatus(204);
 });
 
-app.get("/:variant?/:size?/:name?", (req, res) => {
+app.get("/:size?/:name?", (req, res) => {
   const { variant = DEFAULT_VARIANT, size = DEFAULT_SIZE } = req.params;
   const explicitName = req.query.name || req.params.name;
   const name = explicitName || Math.random().toString();
-  const colors = normalizeColors(req.query.colors || DEFAULT_COLORS);
+
+  // Select a random palette
+  const randomIndex = Math.floor(Math.random() * DEFAULT_COLORS.length);
+
+  const colors = normalizeColors(req.query.colors || DEFAULT_COLORS[randomIndex]);
   const square = req.query.hasOwnProperty("square");
 
   if (!VALID_VARIANTS.has(variant)) {
