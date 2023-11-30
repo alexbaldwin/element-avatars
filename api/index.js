8c4ffad5969cccc69e4fe01640c0609a5bb4276b
Alex Baldwin <alexbaldwin@gmail.com>
2023-11-30 16:33:37 -0600

    Eliminate variant variance

--- api/index.js
+++ api/index.js
@@ -5,8 +5,20 @@ const Avatar = require("boring-avatars").default;
 // const DEFAULT_COLORS = [ "#000", "#EFEFEF", "#000", "#FFF", "#000", ].join(",");
 
 const DEFAULT_COLORS = [
-  ["#000", "#000", "#000", "#000", "#000"].join(","),
+  // Kleurvorm Cool
   ["#7C17F8", "#5200FF", "#E3B5F9", "#F720FF", "#7B8DF5"].join(","),
+  // Kleurvorm Warm
+  ["#FF0C15", "#E764A8", "#D26FE4", "#FC6A2B", "#FFA902"].join(","),
+  // Element Moon
+  ["#EC618F", "#8963BC", "#B583F8", "#81D0FA", "#33759A"].join(","),
+  // Element Sun
+  ["#983A36", "#EB4374", "#EC666E", "#F19A6D", "#F7CF57"].join(","),
+  // Element Water
+  ["#BB6DF7", "#9960E3", "#3E82A7", "#F3AE76", "#B9FD67"].join(","),
+  // Element Ground
+  ["#84D0E4", "#7DE069", "#D2EE51", "#E48B45", "#E9722D"].join(","),
+  // Element Air
+  ["#7E51F6", "#E0C8FB", "#9946F6", "#82BDEA", "#4250A5"].join(","),
   // Add more color arrays as needed
 ];
 
@@ -50,7 +62,7 @@ app.get("/:size?/:name?", (req, res) => {
   const square = req.query.hasOwnProperty("square");
 
   if (!VALID_VARIANTS.has(variant)) {
-    return res.status(400).send("Invalid variant");
+    variant = DEFAULT_VARIANT;
   }
 
   res.setHeader("Content-Type", "image/svg+xml");
